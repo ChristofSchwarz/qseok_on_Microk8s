4567ded287876b98a51e1a379b40cd07253e8488
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-10 19:34:25 +0200

    Update deploy_3_qliksense.sh

--- sh/deploy_3_qliksense.sh
+++ sh/deploy_3_qliksense.sh
@@ -35,40 +35,81 @@ helm repo update
 echo "installing qliksense-init from $QLIK_RELEASE repo using helm ..."
 helm upgrade --install qlikinit "$QLIK_RELEASE/qliksense-init"
 
+# create file identity-providers.json
+echo "[
+    {
+        \"hostname\":\"$HOSTNAME\",
+        \"discoveryUrl\":\"https://$HOSTNAME/auth/realms/master/.well-known/openid-configuration\",
+        \"postLogoutRedirectUri\":\"https://$HOSTNAME/\",
+        \"realm\":\"keycloak\",
+        \"claimsMapping\":{
+            \"groups\":[
+                \"groupmemberships\"
+            ],
+            \"name\":[
+                \"name\",
+                \"preferred_username\"
+            ],
+            \"sub\":[
+                \"preferred_username\"
+            ]
+        },
+        \"clientId\":\"qliklogin\",
+        \"clientSecret\":\"$KEYCLOAKCLIENTSECRET\",
+        \"clockToleranceSec\":3660,
+        \"primary\":true,
+        \"scope\":\"openid profile\"
+    },
+    {
+        \"hostname\":\"$HOSTNAME\",
+        \"issuerConfig\":{
+            \"issuer\":\"https://qlik.api.internal\"
+        },
+        \"primary\":false,
+        \"realm\":\"keycloak\",
+        \"staticKeys\":[
+            {
+                \"kid\":\"my-key-identifier\",
+                \"pem\":\"$(cat public.key | tr "\n" "%" |sed 's/%/\\n/g')\"
+            }
+        ]
+    }
+]">identity-providers.json
+
+
 # Deploy specific version of qlik
 # cat <<EOF | helm upgrade --install qlik $QLIK_RELEASE/qliksense --version 1.31.17 -f -
 
-cat <<EOF | helm upgrade --install qlik $QLIK_RELEASE/qliksense -f -
+echo "
+engine:
+  acceptEULA: "yes"
+
+global:
+  persistence:
+    storageClass: localnfs
+
+mongodb:
+  uri: mongodb://qlik:Qlik1234@mongo-mongodb.default.svc.cluster.local:27017/qsefe?ssl=false  
+
+edge-auth:
+  config:
+    enforceTLS: false
+    secureCookies: false
+    
+elastic-infra:
+  nginx-ingress:
+    controller:
+      service:
+        nodePorts:
+          https: 443
+        type: NodePort
+  
 identity-providers:
   secrets:
-    idpConfigs:
-      - hostname: "$HOSTNAME"
-        realm: "keycloak"
-        primary: true
-        #discoveryUrl: "http://192.168.56.234:32080/auth/realms/master/.well-known/openid-configuration"
-        discoveryUrl: "https://$HOSTNAME/auth/realms/master/.well-known/openid-configuration"
-        postLogoutRedirectUri: "https://$HOSTNAME/"
-        clientId: "qliklogin"
-        clientSecret: "$KEYCLOAKCLIENTSECRET"  # set to the secret you get after creating client in keycloak
-        clockToleranceSec: 3660
-        scope: "openid profile"
-        # useClaimsFromIdToken: true
-        claimsMapping:
-          name: ["name", "preferred_username"]
-          sub: ["preferred_username"]
-          groups: ["groupmemberships"]
-# the 2nd entry in idpConfigs is for API access using JWT tokens. Not needed for keycloak
-#      - hostname: "qlik-shared-vm.q-nnect.net"
-#        realm: "keycloak"
-#        primary: false
-#        issuerConfig:
-#          issuer: "https://qlik.api.internal"
-#        staticKeys:
-#        - kid: "my-key-identifier"
-#          # leave "pem :|-" unchanged and as the last line. The public key will added at
-#          # the end of this file. If you change anything do it above, not below next line!
-#          pem: |-
-EOF
+    idpConfigs: $(cat identity-providers.json|sed 's/\\n/\\\\n/g'|tr -d '\n'|tr -s ' ')
+" >>qliksense.yaml
+
+helm upgrade --install qlik $QLIK_RELEASE/qliksense -f qliksense.yaml
 
 # in order for Edge-Auth to accept an IPD (Keycloak) over https with a self-signed certificate
 # we need to patch its deployment and set environment variable NODE_TLS_REJECT_UNAUTHORIZED=0
