de65253ff4479eeb38aee9ccc8344f9781a849c3
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-10 15:29:38 +0200

    Update deploy_1_requirements.sh

--- sh/deploy_1_requirements.sh
+++ sh/deploy_1_requirements.sh
@@ -86,16 +86,10 @@ sudo service nfs-kernel-server restart
 # sudo apt-get install nodejs -y
 # echo "Installing Node Package Manager npm ..."
 # sudo apt-get install npm -y
-# echo "Installing Json Query tool 'jq' ..."
-# sudo apt-get install jq -y
 
-# mkdir ~/api
-# cp -R /vagrant/api/* ~/api
-# cd ~/api
-
-# echo "Creating private/public key pair in ~/api folder"
-# openssl genrsa -out ~/api/private.key 1024
-# openssl rsa -in ~/api/private.key -pubout -out ~/api/public.key
+echo "Creating private/public key pair"
+openssl genrsa -out private.key 1024
+openssl rsa -in private.key -pubout -out public.key
 
 # echo "Installing NodeJS packages fs, jsonwebtoken"
 # npm install -g fs
