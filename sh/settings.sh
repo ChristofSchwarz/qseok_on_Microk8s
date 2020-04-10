7450e23b6184c886ed35cc923339415b4aa3b60c
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-10 14:34:45 +0200

    Update settings.sh

--- sh/settings.sh
+++ sh/settings.sh
@@ -1,7 +1,11 @@
+#################################################################
 # Global settings which are loaded by all deploy*.sh scripts
+#################################################################
 
-HOSTNAME=qlik-shared-vm.q-nnect.net
-# QLIK_RELEASE can be qlik-stable or qlik-edge 
-QLIK_RELEASE=qlik-stable 
+# Choose which kubernetes version should be used
+KUBERNETES_VERSION="1.15"
+HOSTNAME="qlik-shared-vm.q-nnect.net"
+QLIK_RELEASE="qlik-stable" # can be qlik-stable or qlik-edge 
 
-# Below settings are automatically added. No need to change.
+# In the next lines, settings will be automatically added. No need to change.
+# for example deploy_2 script will add KEYCLOAKCLIENTSECRET below
