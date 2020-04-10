1121b86cc038c35651817d825b835e3f4256e418
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-10 14:30:27 +0200

    Update settings.sh

--- sh/settings.sh
+++ sh/settings.sh
@@ -1,4 +1,7 @@
 # Global settings which are loaded by all deploy*.sh scripts
+
 HOSTNAME=qlik-shared-vm.q-nnect.net
+# QLIK_RELEASE can be qlik-stable or qlik-edge 
+QLIK_RELEASE=qlik-stable 
 
 # Below settings are automatically added. No need to change.
