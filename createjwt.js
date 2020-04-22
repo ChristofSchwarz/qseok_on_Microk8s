21fb5088d3c356d002d3aa16408042708a4f2a2d
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-22 13:27:26 +0200

    Create createjwt.js

---
+++ createjwt.js
@@ -0,0 +1,24 @@
+// Author: Christof Schwarz, christof.schwarz@qlik.com
+//
+// NodeJS app that creates and signs a jwt token for the person ("sub") provided as 
+// command-line argument
+// usage: 
+// nodejs createjwt.js anyuser
+
+var sub = process.argv[2];
+var jwt = require('jsonwebtoken');
+var fs = require('fs');
+var privkey= fs.readFileSync(__dirname + '/private.key');
+var jwt_payload = {
+    iss: "https://qlik.api.internal",
+    aud: "qlik.api",
+    sub: sub,
+    groups : ["Everyone"],
+    name: "Qlik API",
+    exp: 1800000000
+};
+console.log(jwt.sign(jwt_payload, privkey, {
+   algorithm:'RS256',
+   noTimestamp:true,
+   header:{"kid":"my-key-identifier"}
+}));
