53e9e8b76c513ba17706bcfb9537a952ea93ac8b
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-27 15:37:00 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -2,12 +2,15 @@
 # Global settings which are loaded by all deploy*.sh scripts
 #################################################################
 
-# Choose which kubernetes version should be used
-KUBERNETES_VERSION="1.15"
+KUBERNETES_VERSION="1.15"  # Choose which kubernetes version should be used
 HOSTNAME="172.20.16.146"
-QLIK_RELEASE="qlik-stable" # can be "qlik-stable" or "qlik-edge" 
+QLIK_RELEASE="qlik-stable"  # can be "qlik-stable" or "qlik-edge" 
 QLIK_ADMIN_USER="admin"
 QLIK_ADMIN_PWD="Qlik1234"
+MONGO_USER="qlik"
+MONGO_PWD="Qlik1234"
+POSTGRES_USER="pgadmin"
+POSTGRES_PWD="pgadmin"
 
 # In the next lines, settings will be automatically added. No need to change.
 # for example deploy_2 script will add KEYCLOAKCLIENTSECRET below
