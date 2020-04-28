35be79bd1392f7a7f4b10961718c87912ee6f8e4
Christof Schwarz <christof.schwarz@gmx.at>
2020-04-28 17:18:59 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -3,6 +3,7 @@
 #################################################################
 
 KUBERNETES_VERSION="1.15"  # Choose which kubernetes version should be used
+KUBLET_VERSION="1.15.11-00"
 HOSTNAME="172.20.16.146"
 QLIK_RELEASE="qlik-stable"  # can be "qlik-stable" or "qlik-edge" 
 QLIK_ADMIN_USER="admin"
