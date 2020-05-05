5421ce9bee8be9c6f4f69f81df970a2e22484423
Christof Schwarz <christof.schwarz@gmx.at>
2020-05-05 17:32:52 +0200

    Update fix_coredns_azure.sh

--- fix_coredns_azure.sh
+++ fix_coredns_azure.sh
@@ -1,4 +1,9 @@
-
+# Fix for CoreDNS running on an Azure Linux VM by Christof Schwarz
+# the "forward" attribute in Corefile (configmap "coredns") cannot route
+# entries like *.internal.cloudapp.net ... if such addresses are sent to 
+# Google DNS servers like 8.8.8.8 or 8.8.4.4 they are not found.
+# Solution: put Microsofts DNS in first place 168.63.129.16 ...
+# https://docs.microsoft.com/en-us/azure/virtual-network/what-is-ip-address-168-63-129-16
 kubectl get configmap coredns -n kube-system -o yaml >tmp_coredns.yaml
 # insert the Azure DNS server as the first forward IP 
 sed -i 's/forward . /forward . 168.63.129.16 /g' tmp_coredns.yaml
