09094be1ed80ff7dfc50492ca087bcdff5a2bb84
Christof Schwarz <christof.schwarz@gmx.at>
2020-05-05 17:15:12 +0200

    Create fix_coredns_azure.sh

---
+++ fix_coredns_azure.sh
@@ -0,0 +1,9 @@
+
+kubectl get configmap coredns -n kube-system -o yaml >tmp.yaml
+# insert the Azure DNS server as the first forward IP 
+sed -i 's/forward . /forward . 168.63.129.16 /g' tmp.yaml
+# in case this script is run twice, remove a duplicate entry of that IP address
+sed -i 's/forward . 168.63.129.16 168.63.129.16 /forward . 168.63.129.16 /g' tmp.yaml
+kubectl apply -f tmp.yaml
+# Restart the coredns pod to pick up the change
+kubectl delete pod -n kube-system -l k8s-app=kube-dns
