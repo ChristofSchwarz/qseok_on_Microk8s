cac5f20dac401e91118820d2cb09d104325c6050
Christof Schwarz <christof.schwarz@gmx.at>
2020-05-12 13:19:18 +0200

    Update deletepod.sh

--- deletepod.sh
+++ deletepod.sh
@@ -1,4 +1,9 @@
-# deletes all pods that match a certain pattern e.g.
-# sh deletepod.sh ident   ... will delete pod qlik-identity-providers-65bc75d46-s52q5 
-# sh deletepod.sh "edge\|ident" ... will delete pods of 2 matching patterns: edge and ident
-kubectl delete $(kubectl get pod -o=name |grep $1)
+if [ $# -eq 0 ]
+  then
+    echo 'deletes all pods that match a certain pattern ...'
+    echo 'Usage:'
+    echo 'sh deletepod.sh ident   ... will delete pods whose name contain *ident*'
+    echo 'sh deletepod.sh "edge\|ident" ... will delete pods of 2 matching patterns: edge or ident'
+  else
+    kubectl delete $(kubectl get pod -o=name |grep $1)
+fi
